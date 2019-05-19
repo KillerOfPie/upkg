5045c661e6dc8b269cc8aa325322e3d7500f5acd
boussou <boussou@gmail.com>
2019-05-19 07:23:21 +0200

    Added Gentoo, Opensuse, Redhat

--- install.sh
+++ install.sh
@@ -11,10 +11,12 @@ checkos()
 case `uname` in
   Linux )
      LINUX=1
+     which dnf >/dev/null 2>&1  && { echo redhat; return; }
      which yum >/dev/null 2>&1  && { echo centos; return; }
      which zypper >/dev/null 2>&1  && { echo opensuse; return; }
      which apt-get>/dev/null 2>&1  && { echo debian; return; }
      which pacman >/dev/null 2>&1 && { echo arch; return;  }
+     which emerge >/dev/null 2>&1 && { echo gentoo; return;  }
      ;;
   Darwin )
      DARWIN=1
